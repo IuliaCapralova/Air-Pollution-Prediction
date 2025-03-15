9a8f428957e3bdd8c9ff3da0fe6fe472cc241ad8
Iulia Capralova <iuliakapralov@gmail.com>
2025-03-15 21:25:12 +0100

    .

---
+++ data/processed/main.py
@@ -0,0 +1,14 @@
+# to check ranges
+
+import pandas as pd
+
+data = pd.read_csv("forecast_example.csv")
+
+min_values = data.min()
+max_values = data.max()
+
+print("Minimum values for each column:")
+print(min_values)
+
+print("\nMaximum values for each column:")
+print(max_values)
